8333e8ec24f27004d384e26b17fe55e646909b0f
mzijta <marcellazijta@gmail.com>
2023-10-04 15:45:34 +0200

    added test for subtract

--- example.py
+++ example.py
@@ -1,9 +1,13 @@
 def add(a,b):
 	return a+b
 
-def substract(a,b):
+def subtract(a,b):
 	return a+b
 
 def test_add():
 	assert add(1,1) == 2
 	assert add(5.5,3.2) == 8.7
+
+def test_subtract():
+	assert subtract(1,1) == 0
+	assert substract(10,-10) == 20
